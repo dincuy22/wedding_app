ff21b9a66b47db824480a41c8fcbc464e86cbe9d
dincuy22 <dincuy2022@gmail.com>
2022-10-14 14:05:07 +0000

    Cover

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { images } from "../../constants";
+import { BiEnvelopeOpen } from "react-icons/bi";
 
-import "./Cover.scss"
+import "./Cover.scss";
 
 const Cover = () => {
-  return <div>Cover</div>;
+  const [open, setOpen] = useState(false);
+  const handleClick = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
+  return (
+    <div className={`bg-cover ${open ? "hide" : ""}`}>
+      <div className="cover">
+        <img src={images.cover} alt="" width={250} />
+        <h1>Habib & Adiba</h1>
+        <p>Kepada Bapak/Ibu/Saudara/i</p>
+        <div>
+          <span>
+            Kami Mengundang Anda Untuk Hadir Di Acara Pernikahan Kami.
+          </span>
+          <button onClick={handleClick}>
+            <BiEnvelopeOpen />
+            Buka Undangan
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cover;
---
+++ src/constants/images.js
@@ -0,0 +1,4 @@
+import bgCover from "../assets/bg-cover.png";
+import cover from "../assets/cover.png";
+
+export default { bgCover, cover };
